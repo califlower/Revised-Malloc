f5e7076cc34acf93729f007204c5f35723a8b676
Cal <cal13199@gmail.com>
2015-10-14 12:11:25 -0400

    Some updates

--- malloc.c
+++ malloc.c
@@ -1,8 +1,14 @@
 #include "mymalloc.h"
-#define MEMBLOCK 5000
+
 // Use a static char array (static char myblock[5000] for your malloc() and free() to use to manage dynamic memory.
 
 static char myblock[MEMBLOCK]; //big block of memory space
+
+struct memNode
+{
+	struct memNode *next;
+}
+*head;
 
 // Basic function of malloc(size t size) is to return a pointer to a block of the requested size.
 void *mymalloc(unsigned int size, char *file, int line)
--- malloc.h
+++ malloc.h
@@ -1,8 +1,22 @@
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #ifndef MALLOC_H
 #define MALLOC_H
 
-void * my_malloc( unsigned int size, char * file, int line );
-void my_free( void * p, char * file, int line );
+#define malloc( x ) mymalloc( x , __FILE__ , __LINE__ )
+#define free( x ) myfree( x , __FILE__ , __LINE__ )
+
+
+#define MEMBLOCK 5000
+
+
+void * mymalloc( unsigned int size, char * file, int line );
+
+void myfree( void * p, char * file, int line );
 
 // add functions for each detectable errors
+
+#endif
