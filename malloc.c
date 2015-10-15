a8f3d207d541a614c4d11599ebe184dda16ce575
Cal <cal13199@gmail.com>
2015-10-15 15:59:43 -0400

    Added to Malloc

--- malloc.c
+++ malloc.c
@@ -1,27 +1,56 @@
 #include "malloc.h"
 #include <stdio.h>
 
-// Use a static char array (static char myblock[5000] for your malloc() and free() to use to manage dynamic memory.
+
 
 #define TOTALMEMORY 5000
 
-struct MemoryBlock *head;
-static char mymemorystoragearray[TOTALMEMORY]; //big block of memory space
 
+static char block[TOTALMEMORY]; 
 
-void *mymalloc(unsigned int size, char *file, int line){
-	
-	if (size == 0) {
-		fprintf(stderr, "Unable to allocate 0 bytes in FILE: '%s' on LINE: '%d'\n", file, line);
+int findBlock ()
+{
+	if (head==NULL)
 		return 0;
+	else
+	{
+		return 2;
 	}
-
-return 0;
 }
 
-void myfree(void *p, char *file, int line);
 
- int main(int argc, char **argv) {
+void *mymalloc(unsigned int size, char *file, int line)
+{
+	
+	if (size == 0) 
+	{
+		fprintf(stderr, "Unable to allocate 0 bytes in FILE: '%s' on LINE: '%d'\n", file, line);
+		
+		return 0;
+	}
+	else if (head==NULL)
+	{
+		struct MemoryBlock *entry;
+		
+		entry->size=size;
+		entry->next=NULL;
+		entry->isFree=0;
+		
+		void *pointer= (void *)&block[sizeof(entry)+size];
+		return pointer;
+		
+	}
+
+	return 0;
+}
+
+void myfree(void *p, char *file, int line)
+{
+	
+}
+
+ int main(int argc, char **argv) 
+ {
   
   return 0;
  }
--- malloc.h
+++ malloc.h
@@ -2,14 +2,15 @@
 #define MALLOC_H
 
 void * my_malloc( unsigned int size, char * file, int line );
+
 void my_free( void * p, char * file, int line );
 
 
 struct MemoryBlock
 {
 	struct MemoryBlock *next;
-	int isfree; // flag which is used to tell whether the program is freed or not
-	int size; // size of the block of memory
-};
+	int isFree; 
+	int size; 
+}*head;
 
-void MyFree(void* ptr);
+
