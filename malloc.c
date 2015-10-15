9096cb8d71ba295e7c8cf9087d2eaa53be4431cc
saif429 <saif429@gmail.com>
2015-10-14 22:01:33 -0400

    Update malloc.c

--- malloc.c
+++ malloc.c
@@ -1,24 +1,27 @@
-#include "mymalloc.h"
+#include "malloc.h"
+#include <stdio.h>
 
 // Use a static char array (static char myblock[5000] for your malloc() and free() to use to manage dynamic memory.
 
-static char myblock[MEMBLOCK]; //big block of memory space
+#define TOTALMEMORY 5000
 
-struct MemEntry
-{
-	struct MemEntry *prev, *next;
-	int isfree;		// 1 - yes, 0 - no
-	int size;
-};
-*head;
+struct MemoryBlock *head;
+static char mymemorystoragearray[TOTALMEMORY]; //big block of memory space
 
-// Basic function of malloc(size t size) is to return a pointer to a block of the requested size.
-void *mymalloc(unsigned int size, char *file, int line)
-{
 
+void *mymalloc(unsigned int size, char *file, int line){
+	
+	if (size == 0) {
+		fprintf(stderr, "Unable to allocate 0 bytes in FILE: '%s' on LINE: '%d'\n", file, line);
+		return 0;
+	}
+
+return 0;
 }
-// function returns the allocated block to the memory resource, making it available to use in later malloc() calls.
-void myfree(void *p, char *file, int line){
 
+void myfree(void *p, char *file, int line);
 
-}
+ int main(int argc, char **argv) {
+  
+  return 0;
+ }
