ddb5e75e5af12cea6dcb5e5da1dee3970280cdd1
saif429 <saif429@gmail.com>
2015-10-16 17:15:58 -0400

    Update main2.c

--- main2.c
+++ main2.c
@@ -1,15 +1,15 @@
 #include "malloc.c"
 int main(int argc, char **argv) 
 {
-/* 
+
  printf("----------------------------------------------------\n");
 
  printf("Test Case 1: (Allocating Memory for an Int)\n");
 
 	int *p=malloc(sizeof(int));
 	*p=30;
-	printf("%i\n", *p);
 	free(p);
+	printf("%i\n", *p);
  printf("----------------------------------------------------\n");
  printf("Test Case 2: (Allocating Memory for a Double)\n");
 
@@ -36,34 +36,5 @@ printf("Test Case 5: (Allocating To Much Memory )\n");
 printf("----------------------------------------------------\n");
 printf("Test Case 6: (Freeing Space To Add Memory )\n");	
 
-	return 0;*/
-
-	int *p=malloc(sizeof(int));
-	*p=30;
-	int *x=malloc(sizeof(int));
-	*x=2;
-	int *y=malloc(sizeof(int));
-	*y=1;
-	int *m=malloc(sizeof(int));
-	*m=40;
-	int *n=malloc(sizeof(int));
-	*n=40;
-	int *a=malloc(sizeof(int));
-	*a=40;
-	int *u=malloc(sizeof(int));
-	*u=40;
-
-	free(u);
-	int *z=malloc(2000*sizeof(int));
-	int *s=malloc(2000*sizeof(int));
-	*s=5;
-	printf("%i\n", *s);
-	printf("%i\n", *p);
-	printf("%i\n", *x);
-	printf("%i\n", *y);
-	printf("%i\n", *m);
-	printf("%i\n", *n);
-	
-	return 0;
-	
+return 0;
  }
