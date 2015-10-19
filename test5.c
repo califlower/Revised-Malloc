292ac5e4d63b1c9264d6d2e2be4e9eed6ac7be1b
saif429 <saif429@gmail.com>
2015-10-19 15:15:06 -0400

    Update test5.c

--- test5.c
+++ test5.c
@@ -6,10 +6,7 @@ int main(int argc, char **argv)
 printf("Test Case 5: (Allocating To Much Memory )\n");
 	
 	char *d=malloc(10000*sizeof(char));
-	*d='d';
-	printf("%c\n", *d);
-	
-	
+
   printf("Test Case 5: (Allocated To Much Memory Test Cases)\n");
 
 	return 0;
