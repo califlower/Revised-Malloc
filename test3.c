8a3eb9b5c66c54142c52fb22f667364b00c109b1
saif429 <saif429@gmail.com>
2015-10-18 23:32:59 -0400

    Update test3.c

--- test3.c
+++ test3.c
@@ -5,30 +5,23 @@ int main(int argc, char **argv)
  	
   printf("Test Case 3: (Allocating Memory for a Character)\n");
 
-	char *z=malloc(sizeof(char*));
+	char *z=malloc(10*sizeof(char));
 	*z='z';
-	char *x=malloc(sizeof(char));
-	*x='x';
-	char *y=malloc(sizeof(char));
-	*y='y';
-	char *m=malloc(sizeof(char));
-	*m='m';
-	char *n=malloc(sizeof(char));
-	*n='n';
-	char *a=malloc(sizeof(char));
-	*a='a';
-	char *u=malloc(sizeof(char));
-	*u='u';
-	
 	printf("%c\n", *z);
-	printf("%c\n", *x);
-	printf("%c\n", *y);
-	printf("%c\n", *m);
-	printf("%c\n", *n);
-  	printf("%c\n", *a);
-	printf("%c\n", *u);
-
-  printf("Test Case 2: (Allocated Memory for All Character Test Cases)\n");
+	
+	char *d=malloc(100*sizeof(char));
+	*d='d';
+	printf("%c\n", *d);
+	
+	char *f=malloc(100*sizeof(char));
+	*f='f';
+	printf("%c\n", *f);
+	
+	char *j=malloc(1000*sizeof(char));
+	*j='j';
+	printf("%c\n", *j);
+	
+  printf("Test Case 3: (Allocated Memory for All Character Test Cases)\n");
 
 	return 0;
  }
