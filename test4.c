e83e507bf022bb2c9b8a3c0cad5f13ddda16da44
saif429 <saif429@gmail.com>
2015-10-19 11:16:18 -0400

    Update test4.c

--- test4.c
+++ test4.c
@@ -1,27 +1,31 @@
-#include "malloc.h"
+#include "malloc.c"
+
 int main(int argc, char **argv) 
  {
-	int *p=malloc(sizeof(int));
-	*p=30;
-	int *x=malloc(sizeof(int));
-	*x=2;
-	int *y=malloc(sizeof(int));
-	*y=1;
-	int *m=malloc(sizeof(int));
-	*m=40;
-	int *n=malloc(sizeof(int));
-	*n=40;
-	int *a=malloc(sizeof(int));
-	*a=40;
-	int *u=malloc(sizeof(int));
-	*u=40;
-
-	free(u);
-	printf("%i\n", *p);
-	printf("%i\n", *x);
-	printf("%i\n", *y);
-	printf("%i\n", *m);
-	printf("%i\n", *n);
+ 	
+  printf("Test Case 4: (Allocating A lot of Memory )\n");
+	int *ptr;
+	ptr=(int*)malloc(500*sizeof(int));
+	*ptr=5;
+	printf("%i\n", *ptr);
 	
+	char *d=malloc(100*sizeof(char));
+	*d='d';
+	printf("%c\n", *d);
+	
+	char *f=malloc(100*sizeof(char));
+	*f='f';
+	printf("%c\n", *f);
+	
+	char *j=malloc(1000*sizeof(char));
+	*j='j';
+	printf("%c\n", *j);
+	
+	char *x=malloc(1000*sizeof(char));
+	*x='x';
+	printf("%c\n", *x);
+	
+  printf("Test Case 4: (Allocated Memory for Big Memory Test Cases)\n");
+
 	return 0;
  }
